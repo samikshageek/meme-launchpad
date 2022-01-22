22a82b5809bdfbc9a2d4d9584aa1442af773c665
Samiksha <sammypcs12@gmail.com>
2022-01-22 13:17:10 +0530

    meme content screen with ScrollView added

--- components/MemeScreen.js
+++ components/MemeScreen.js
@@ -1,15 +1,79 @@
 import React from "react";
-import { Text ,View , StyleSheet } from 'react-native'
+import { Text ,View , StyleSheet ,SafeAreaView, ScrollView, Dimensions} from 'react-native'
+import { Card , Avatar ,Button ,Title} from 'react-native-paper';
 
+
+const screenWidth= Dimensions.get('window').width;
+const screenHeight= Dimensions.get('window').height ;
 const MemesDisplayScreen = (props)=>{
 
+
    return(
-   
-       <View>
-         <Text>Radhe Radhe</Text>
-       
+     <SafeAreaView >
+       <ScrollView >
+         <View style={Styles.safeArea}>
+       <View style={Styles.container}>
+         
+         <Card style={Styles.card}>
+           <Card.Cover source={{ uri : "https://c.tenor.com/ZDM5ya38ckcAAAAd/golden-girls-weekend.gif"}} />
+         </Card>
+         
+         
+         <Card style={Styles.card}>
+            <Card.Cover source={{uri : "https://c.tenor.com/DAwDaIviK6AAAAAC/yeah-baby-funny.gif"}} />
+         </Card>
+
+           
+         <Card style={Styles.card}>
+           <Card.Cover source={{ uri : "https://c.tenor.com/ZDM5ya38ckcAAAAd/golden-girls-weekend.gif"}} />
+         </Card>
+
+         <Card style={Styles.card}>
+            <Card.Cover source={{uri : "https://c.tenor.com/DAwDaIviK6AAAAAC/yeah-baby-funny.gif"}} />
+         </Card>
+         
+         <Card style={Styles.card}>
+           <Card.Cover source={{ uri : "https://c.tenor.com/ZDM5ya38ckcAAAAd/golden-girls-weekend.gif"}} />
+         </Card>
+
+         <Card style={Styles.card}>
+            <Card.Cover source={{uri : "https://c.tenor.com/DAwDaIviK6AAAAAC/yeah-baby-funny.gif"}} />
+         </Card>
+
+         <Card style={Styles.card}>
+           <Card.Cover source={{ uri : "https://c.tenor.com/ZDM5ya38ckcAAAAd/golden-girls-weekend.gif"}} />
+         </Card>
+         
        </View>
+       </View>
+       </ScrollView>
+    </SafeAreaView>
    )
 }
 
 export default MemesDisplayScreen ;
+
+const Styles = StyleSheet.create({
+  safeArea:{
+   flex:1
+  },
+  scrollView: {
+   
+    backgroundColor: 'green',
+    marginHorizontal: 5,
+  },
+  container:{
+   display:"flex",
+   flexDirection:"row",
+   flexWrap:"wrap",
+   margin : 10,
+   
+  },
+  card:{
+    height: screenHeight/4,
+    width: "50%",
+    position:"relative",
+    borderColor:"orange",
+   borderWidth:5
+  }
+})
