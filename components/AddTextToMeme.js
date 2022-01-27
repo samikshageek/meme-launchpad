85859189b80efd346821457e8ad9a69486f553f3
Samiksha <sammypcs12@gmail.com>
2022-01-27 13:40:00 +0530

    Download share buttons added

--- components/AddTextToMeme.js
+++ components/AddTextToMeme.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from "react";
-import { Text , View ,StyleSheet , Dimensions} from 'react-native';
-import { Card , Title, TextInput} from 'react-native-paper';
+import { Text , View ,StyleSheet , Dimensions, ScrollView} from 'react-native';
+import { Card , Title, TextInput, ToggleButton , Button} from 'react-native-paper';
 
 const screenHeight = Dimensions.get('screen').height;
 const screenWidth = Dimensions.get('screen').width ;
@@ -9,17 +9,31 @@ const AddText = (props) =>{
     const pickedMeme = props.navigation.route.params.pickedMeme;
     const [topText , setTopText] = useState("");
     const [bottomText , setBottomText] = useState("");
+    const [toggleValue , setToggleValue] = useState('left')
 
     return(
         <>
         {console.log("pickedMeme", pickedMeme)}
-        <View style = {Styles.container}>
+        <ScrollView style = {Styles.container}>
         <TextInput type="outlined" label ="Enter Top Text" value={topText} onChangeText={text => setTopText(text)} style={{marginBottom:20}} s/>
-        <Card style={Styles.card}>
-           <Card.Cover source={{ uri : pickedMeme}} />
-        </Card>
-        <TextInput type ="outlined" label ="Enter Bottom Text" value={bottomText} onChangeText={text => setBottomText(text)} style={{marginTop:20}} />
+        <View style={{flexDirection:"row"}}> 
+           <Card style={Styles.card}>
+              <Card.Cover source={{ uri : pickedMeme}} />
+           </Card>
+          <View style={Styles.toggleButtons} >
+           <ToggleButton.Group value={toggleValue} onValueChange={value => setToggleValue(value)}>
+             <ToggleButton icon ="share" value="left" />
+             <ToggleButton icon ="download" value="right" />
+             <ToggleButton icon="star" value="" />
+           </ToggleButton.Group>
+           </View>
         </View>
+        <TextInput type ="outlined" label ="Enter Bottom Text" value={bottomText} onChangeText={text => setBottomText(text)} style={{marginTop:20}} />
+         <View style={Styles.buttons}>
+         <Button icon="plus" mode="outlined">Add Text </Button>
+         <Button icon="eye" mode="outlined">Preview</Button>
+         </View>
+         </ScrollView>
         </>
     )
 
@@ -29,14 +43,23 @@ export default AddText;
 
 const Styles= StyleSheet.create({
     card:{
-        height: screenHeight/4,
-        width: screenWidth /2,
-        
+       width: screenWidth - (screenWidth/3),
+       height: screenHeight/4,
+       
+       
     },
     container :{
         flex:1,
-        margin:20,
-        
+        margin:10
+    },
+    toggleButtons:{
+        paddingLeft:20,
+        paddingRight:10
+    },
+    buttons:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        marginTop:20
     }
     
 })
