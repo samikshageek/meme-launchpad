82b942e5b420e6d764daef4ffe5fa6bba983fb8f
Samiksha <sammypcs12@gmail.com>
2022-02-03 21:42:28 +0530

    Modified card template

--- Wdgets/CategoriesCardWidget.js
+++ Wdgets/CategoriesCardWidget.js
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import { View ,Text, Image} from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
-
+import { useNavigation } from '@react-navigation/native';
 
     
 
 const CardComponent = (props) =>{
+    
+    const navigation = useNavigation();
+    let currentItem = Object.keys( props.category);
+    let indexOfSpace = currentItem[0].indexOf(' ') ;
     return(
 
     <View>
          {/* {console.log("props.Item", props.Item)} */}
-         {console.log("props.temp", props.category[Object.keys(props.category)], Object.keys(props.category) ) }
+         {console.log("props.temp", props.category[Object.keys(props.category)], Object.keys(props.category) , currentItem) }
           
          <Card style={{ marginBottom: "5%"}}>
           <Card.Content>
@@ -19,7 +23,7 @@ const CardComponent = (props) =>{
           </Card.Content>
           <Card.Cover source={{ uri: props.category[ Object.keys( props.category ) ] }} />
           <Card.Actions>
-           <Button>Select</Button>
+           <Button onPress={() => navigation.navigate('Memes', {memeSelected: currentItem[0].substring(0 , indexOfSpace) })}>Select</Button>
         
           </Card.Actions>
            </Card>
--- components/MemeCategories.js
+++ components/MemeCategories.js
@@ -1,4 +1,4 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
 import {Button , Text , View , SafeAreaView , ScrollView , StyleSheet , Image , ImageBackground, Dimensions} from 'react-native';
 import ImgLayout from './MemeImageLayout';
 
@@ -6,13 +6,55 @@ const background = { uri: "https://reactjs.org/logo-og.png" };
 const screenWidth = Dimensions.get('window').width ;
 const screenHeight = Dimensions.get('window').height ;
 
-let category=[{"Trending Now": "https://c.tenor.com/olNJM0xe5oAAAAAC/working-from-home-wfh.gif"},
-              {"Popular Templates": "https://c.tenor.com/uPUduP9YZegAAAAC/sleepy-tired.gif"},
-               {"Blank Templates": "https://c.tenor.com/s8JMqoY6-CoAAAAi/crayon-shinchan.gif"},
-               {"Classic Templates": "https://c.tenor.com/OQJmvVwLZ5YAAAAi/tkthao219-bubududu.gif"}]
+
 
 const MemeCategory = (props) => {
   
+  const [category, setCategory]= useState([
+    {"Trending Now": "https://c.tenor.com/olNJM0xe5oAAAAAC/working-from-home-wfh.gif"},
+  {"Popular Templates": "https://c.tenor.com/uPUduP9YZegAAAAC/sleepy-tired.gif"},
+   {"KidsFavourite Templates": "https://c.tenor.com/s8JMqoY6-CoAAAAi/crayon-shinchan.gif"},
+   {"Classic Templates": "https://c.tenor.com/OQJmvVwLZ5YAAAAi/tkthao219-bubududu.gif"},
+   {"Birthday Templates": "https://c.tenor.com/S7EwxHkrEFEAAAAC/happy-birthday-sparkle.gif"}])
+
+   const requestOptions={
+     method:"POST",
+     headers: { 'Content-Type': 'application/json' },
+   }
+
+   let templates = [];
+
+   const fetchCategoriesTemplate = async(item) => {
+     let currentItem = Object.keys( item);
+     let indexOfSpace = currentItem[0].indexOf(' ') ;
+
+    
+     console.log("memeTypeToFetch ",currentItem[0] , currentItem[0].indexOf(' '), currentItem[0].substring(0 , indexOfSpace) )
+
+       await fetch("https://fun-meme-api.herokuapp.com/memes/ExploreTemplate", {
+         method:"POST",
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify( {memeType: currentItem[0].substring(0 , indexOfSpace) + "Template"} )
+         
+       }).then(response => {
+         response.json()
+         .then(data => {
+           console.log("d",data);
+           templates.push({[currentItem[0]] : data.url});
+
+           if(templates.length === 5){
+             setCategory(templates)
+           }
+          })})
+         
+   }
+
+   useEffect(() =>{
+     
+     category.map(fetchCategoriesTemplate);
+
+   },[])
+
     return(
       
  <SafeAreaView > 
